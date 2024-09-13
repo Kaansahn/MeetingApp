c9506459f7333f77bd2b2173704e2ae796b70685
Kaan <kaan.sahin2@hotmail.com>
2024-09-13 21:40:09 +0300

    Form Validations

--- obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820041ce173c6544ae2015190fbe251b11a233a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f48a073b52a8ffb5c3c390d6611cc700dbec44ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
