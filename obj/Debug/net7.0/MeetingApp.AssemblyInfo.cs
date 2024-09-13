ec83c9bff9549a7bea8f5106ef9376dcc6d2a1eb
Kaan <kaan.sahin2@hotmail.com>
2024-09-13 16:22:22 +0300

    Model Binding

--- obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9de76fb84aa80631337f47127a5e847a4e5cfe3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33265188527a45acc75ca1b9407fa1c278982fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
