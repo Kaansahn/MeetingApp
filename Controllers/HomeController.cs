0f7844e8feba7dcb5991524c38d5bb37aad0d319
Kaan <kaan.sahin1@bahcesehir.edu.tr>
2024-09-13 09:50:47 +0300

    mvc is set. homecontroller and meetincontroller are added.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index(){
+            return "home/index";
+        }
+    }
+}
